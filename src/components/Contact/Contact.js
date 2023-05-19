1ee5ed2ee9710ac1696dc35d6b787e4afb5c3823
pabon98 <hasinanjumpabon@gmail.com>
2023-05-20 01:03:58 +0600

    contact page design changed

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -2,117 +2,162 @@ import React from "react";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Navbar from "../Navbar/Navbar";
+import "./Contact.css";
+import useAuth from "../hooks/useAuth/useAuth";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const Contact = () => {
+  const { user } = useAuth();
+
+    /* Toast Message */
+    const notify =()=>{
+      toast.success('Thanks for your response!', {
+        position: "top-center",
+        autoClose: 1000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+    }
+
+  const handleSubmit =(e)=>{
+  e.preventDefault()
+
+  //check if all the input fields have value
+  
+  const form = document.getElementById('newContactForm');
+  const formData = new FormData(form);
+  const formValues = Object.fromEntries(formData.entries());
+
+  const { userName, email, subject, city, contact } = formValues;
+
+  if (userName && email && subject && contact && city) {
+    // Show success toast message
+    notify();
+  } else {
+    // Show error toast message
+    toast.error('Please fill up all the fields.', {
+      position: 'top-center',
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: 'light',
+    });
+  }
+   
+  }
+
   return (
     <div>
       <Navbar></Navbar>
-      <Header></Header>
-      <div className="container ">
-        <h1>Contact Us</h1>
-        <div className="row d-flex justify-content-center align-items-center mt-4">
-          <div className="col-md-4 shadow bg-info ">
-            <h2>Why You Choose Us</h2>
-            <p>Best Price Guarantee</p>
-            <p>Customer care available 24/7</p>
-            <p>5 to 10 years warrenty for parts</p>
-            <p>Best services among the area</p>
-          </div>
-          <div className="col-md-4 shadow bg-info ms-5">
-            <h2>GET A QUESTION?</h2>
-            <p>
-              Do not hesitage to give us a call. We are an <br /> expert team
-              and we are happy to talk to you. <br />
-              And we are here always to help you
-            </p>
-            <p>
-              {" "}
-              <i className="fas fa-envelope"></i> vehica123@gmail.com
-            </p>
-            <p>
-              {" "}
-              <i className="fas fa-phone-alt"></i> +88578546525
-            </p>
-          </div>
-        </div>
-
-        <form className="row g-3 mt-5">
-          <div className="col-md-6">
-            <label for="inputName4" className="form-label">
-              Name
-            </label>
-            <input type="name" className="form-control" id="inputEmail4" />
-          </div>
-          <div className="col-md-6">
-            <label for="inputPassword4" className="form-label">
-              Email
-            </label>
-            <input type="email" className="form-control" id="inputEmail4" />
-          </div>
-          <div className="col-md-6">
-            <label for="inputPhone4" className="form-label">
-              Phone
-            </label>
-            <input type="phone" className="form-control" id="inputPhone4" />
-          </div>
-          <div className="col-md-6">
-            <label for="inputServices4" className="form-label">
-              Services
-            </label>
-            <input type="phone" className="form-control" id="inputServices4" />
-          </div>
-          <div className="col-12">
-            <label for="inputAddress" className="form-label">
-              Message
-            </label>
-            <input
-              type="text"
-              className="form-control"
-              id="inputAddress"
-              placeholder="Message"
-            />
-          </div>
-
-          <div className="col-md-6">
-            <label for="inputCity" className="form-label">
-              City
-            </label>
-            <input type="text" className="form-control" id="inputCity" />
-          </div>
-          <div className="col-md-4">
-            <label for="inputState" className="form-label">
-              State
-            </label>
-            <select id="inputState" className="form-select">
-              <option selected>Choose...</option>
-              <option>...</option>
-            </select>
-          </div>
-          <div className="col-md-2">
-            <label for="inputZip" className="form-label">
-              Zip
-            </label>
-            <input type="text" className="form-control" id="inputZip" />
-          </div>
-          <div className="col-12">
-            <div className="form-check">
-              <input
-                className="form-check-input"
-                type="checkbox"
-                id="gridCheck"
-              />
-              <label className="form-check-label" for="gridCheck">
-                Check me out
-              </label>
+      {/* <Header></Header> */}
+      <div class="container mt-5 shadow">
+        <div class="row ">
+          <div class="col-md-4 bg-primary p-5 text-white order-sm-first order-last">
+            <h2>Let's get in touch</h2>
+            <p>We're open for any suggestion or just to have a chat</p>
+            <div class="d-flex mt-2">
+              <i class="bi bi-geo-alt"></i>
+              <p class="mt-3 ms-3">
+                Address : Road 198 West 21th Street, Suite 721 Singapor WW 10016
+              </p>
+            </div>
+            <div class="d-flex mt-2">
+              <i class="bi bi-telephone-forward"></i>
+              <p class="mt-4 ms-3">Phone : 8888888888</p>
+            </div>
+            <div class="d-flex mt-2">
+              <i class="bi bi-envelope"></i>
+              <p class="mt-4 ms-3">Email : contactform@gmail.com</p>
+            </div>
+            <div class="d-flex mt-2">
+              <i class="bi bi-youtube"></i>
+              <p class="mt-4 ms-3">Channel : www.contactform.com</p>
             </div>
           </div>
-          <div className="col-12">
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
+          <div class="col-md-8 p-5 ">
+            <h2>Get in touch</h2>
+            {/* Contact Form */}
+            <form id="newContactForm" onSubmit={handleSubmit} class="row g-3 contactForm mt-4">
+              <div class="col-12">
+                <label for="inputEmail4" class="form-label">
+                  Name
+                </label>
+                <input
+                  type="name"
+                  name="userName"
+                  value={user.displayName}
+                  class="form-control"
+                  id="inputEmail4"
+                  required
+                />
+              </div>
+              <div class="col-12">
+                <label for="inputAddress" class="form-label">
+                  Email Id
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  value={user.email}
+                  class="form-control"
+                  id="inputAddress"
+                  required
+                />
+              </div>
+              <div class="col-12">
+                <label for="inputAddress" class="form-label">
+                  Subject
+                </label>
+                <input
+                  type="text"
+                  name="subject"
+                  class="form-control"
+                  id="inputAddress"
+                  required
+                />
+              </div>
+              <div class="col-md-6">
+                <label for="inputCity" class="form-label">
+                  City
+                </label>
+                <input type="text" name="city" class="form-control" id="inputCity" />
+              </div>
+              <div class="col-md-6">
+                <label for="Number" class="form-label">
+                  Contact Number
+                </label>
+                <input type="text" name="contact" class="form-control" id="Number" required />
+              </div>
+              <div class="col-12">
+                <button onClick={handleSubmit}  type="submit" class="btn btn-primary mt-3">
+                  Submit
+                </button>
+                <ToastContainer
+                  position="top-center"
+                  autoClose={1000}
+                  hideProgressBar
+                  newestOnTop={false}
+                  closeOnClick
+                  rtl={false}
+                  pauseOnFocusLoss
+                  draggable
+                  pauseOnHover
+                  theme="light"
+                />
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
       </div>
+
       <Footer></Footer>
     </div>
   );
