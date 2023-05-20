2ffeb592523e48c19d020b2e5d6c00a268374cc6
pabon98 <hasinanjumpabon@gmail.com>
2023-05-20 10:38:20 +0600

    background image added for hero Banner

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,17 +6,17 @@ const Header = () => {
     return (
         <div className="header">
            <div className="py-5">
-              <br /><br /><br /><br /><br /> <br /> <br />
-          <div className="header-content">
-          <h3 className='text-white fs-1'>Modern Classic Decorative</h3>
-          <p className='text-white fs-2'>Get 20% off on selected items</p>
+              <br /><br />
+          <div className="mb-3">
+          <h3  className='text-white fs-1 mt-4'>Find Your Perfect Car</h3>
+          <p  className='text-white fs-3'>with thousands of car we have just te right one for you</p>
           </div>
            <div>
-           <p className="text-white header-content fs-3">We can help you find the best car. Check our reviews, compare models and find cars for sale.</p>
+           {/* <p className="text-white header-content fs-3">We can help you find the best car. Check our reviews, compare models and find cars for sale.</p> */}
            <Link to="/loginform">
-           <button className="btn btn-warning p-2">Purchase Now</button>
+           <button  className="btn me-auto btn-warning p-2">Purchase Now</button>
            </Link>
-           <br /> <br /> <br /> <br /> <br /> <br /> <br /> <br />
+           <br /> <br /> <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
            </div>
             
            </div>
